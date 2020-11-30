0cef562ea67cf969e3c7472edc552be8e0e53923
terencecistudent <terencecistudent@users.noreply.github.com>
2020-11-30 01:25:30 +0000

    Commented unused code removed

--- photos/models.py
+++ photos/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Photo(models.Model):
     title = models.CharField(max_length=50, null=True)
--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from photos.models import Photo
 
-# Create your views here.
 
 def index(request):
     photos = Photo.objects.all()
